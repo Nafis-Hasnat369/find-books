ea0dafaa5f992021cccf11043c4a6029104ea69e
Nafis-Hasnat369 <nafis7823@gmail.com>
2021-09-02 02:15:08 +0600

    loading  spinner added

--- js/book-archive.js
+++ js/book-archive.js
@@ -2,22 +2,29 @@ const searchBook = async () => {
     // get input
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
+
     // clear search field
     searchField.value = "";
 
     // clear previous result
     const totalResult = document.getElementById("total-result");
     totalResult.textContent = "";
-    // search result
+
+    // clear previous search result
+    const searchResult = document.getElementById("search-result");
+    searchResult.textContent = "";
+
+    // load data
     if (searchText === "") {
-        const searchResult = document.getElementById("search-result");
-        searchResult.textContent = "";
         const div = document.createElement('div');
         div.classList.add("mx-auto");
-        div.innerHTML = "<h5>Write something to search...</h5>";
+        div.innerHTML = "<h5>Please write to search...</h5>";
         searchResult.appendChild(div);
     }
     else {
+        // loding animation/time
+        loadAnimation("block");
+
         // load data
         const url = `https://openlibrary.org/search.json?q=${searchText}`;
         const res = await fetch(url);
@@ -25,15 +32,16 @@ const searchBook = async () => {
         displaySearchResult(data);
     }
 };
+
 // display search results
 const displaySearchResult = books => {
     const searchResult = document.getElementById("search-result");
-    searchResult.textContent = "";
     if (books.docs.length == "0") {
         const div = document.createElement('div');
         div.classList.add("mx-auto");
-        div.innerHTML = "<h5>No results found!</h5>";
+        div.innerHTML = "<h5>No results found, try different keywords!</h5>";
         searchResult.appendChild(div);
+        loadAnimation("none");
     }
     else {
         books.docs.forEach(book => {
@@ -48,12 +56,19 @@ const displaySearchResult = books => {
                     <h4 class="card-title">Publisher:<span class="text-success"> ${book.publisher}</span></h4>
                     <h4 class="card-title">First Published:<span class="text-info"> ${book.first_publish_year}</span></h4>
                 </div>
-            </div>
-            `;
+            </div>`;
             searchResult.appendChild(div);
+
             // display total result
             const totalResult = document.getElementById("total-result");
             totalResult.innerHTML = `<h3>Total search result: <span class="text-primary">${books.numFound}</span></h3>`;
+            loadAnimation("none");
         })
     }
 };
+
+// load animation
+const loadAnimation = view => {
+    document.getElementById("animation").style.display = view;
+};
+loadAnimation("none");
